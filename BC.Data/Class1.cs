22ab87982e0f5ab571ec5a3ea535000bb3c38cb4
Queenfrostbitten <crowndevnetwork@gmail.com>
2024-12-12 00:07:31 -0500

    finally redo

---
+++ BC.Data/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bookingcalendar.Capstone
+{
+    public class BookingCalendar : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public BookingCalendar(DbContextOptions<BookingCalendar> options) : base(options) { }
+    }
+
+    public class Event
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string? Text { get; set; }
+        public string? Color { get; set; }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Bookingcalendar.Capstone;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BookingCalendar>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CalendarContext")));
